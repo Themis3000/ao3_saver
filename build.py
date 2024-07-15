b1da03863520dd9289ac5c5fefc462ad69b9ffe3
Themi Megas <Tcm4760@gmail.com>
2024-07-15 02:49:51 -0500

    added build script

---
+++ build.py
@@ -0,0 +1,22 @@
+import zipfile
+import os
+
+chrome_zf = zipfile.ZipFile('chrome.zip', 'w')
+firefox_zf = zipfile.ZipFile('firefox.zip', 'w')
+for dirname, subdirs, files in os.walk("./"):
+    chrome_zf.write(dirname)
+    firefox_zf.write(dirname)
+    for filename in files:
+        if filename.endswith(".py") or filename.endswith(".zip"):
+            continue
+        if filename == "manifest.json":
+            chrome_zf.write(os.path.join(dirname, filename))
+            continue
+        if filename == "manifest-old.json":
+            firefox_zf.write(os.path.join(dirname, filename), "manifest.json")
+            continue
+        chrome_zf.write(os.path.join(dirname, filename))
+        firefox_zf.write(os.path.join(dirname, filename))
+
+chrome_zf.close()
+firefox_zf.close()
